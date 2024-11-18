0e8d432a9d0ea4d452e7af49469eed7287625c5a
francescoboc <francescoboc@gmail.com>
2024-11-18 16:27:11 +0100

    first commit

---
+++ select_file.py
@@ -0,0 +1,58 @@
+pi = 3.141
+
+# trust parameter
+# trust = 0.0
+
+trust = 0.1
+# trust = 0.2
+# trust = 0.3
+# trust = 0.4
+# trust = 0.5
+# trust = 0.6
+# trust = 0.7
+# trust = 0.8
+# trust = 0.9
+
+# trust = 1.0
+
+# check only center of mass or consider all the agents
+center_of_mass = 0
+
+# swarm parameters
+rd = 0.2
+spawn_radius = 25*rd 
+
+# visual_radius = 0
+# visual_radius = 5*rd
+visual_radius = 2*spawn_radius
+
+# random initial casting clock
+rand_casting_steps = 100
+# rand_casting_steps = 20
+# rand_casting_steps = 0
+
+# average angle of initial distribution
+mu = 0
+# mu = pi/4
+
+# std of initial distribution
+sigma = 0
+# sigma = pi/3
+# sigma = pi/2
+# sigma = pi
+
+# n_runs = 50
+n_runs = 2
+
+# final_time = 500
+final_time = 0
+
+final_x = 100
+
+# distance from source and shift from centerline
+l_x = 35
+h_y = 8
+
+# hexbin parameters
+gridsize = 200
+offset = 100
