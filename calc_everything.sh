00628cfcb3ff0b1112df1920c88c564c210a441d
francescoboc <francescoboc@gmail.com>
2025-01-08 16:28:13 +0100

    first commit

---
+++ calc_everything.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Run scripts in the background on different threads
+python copy_wt_history.py &
+PID1=$!
+sleep 0.5
+echo '' 
+
+python calc_first_passage.py &
+PID2=$!
+sleep 0.5
+echo '' 
+
+python calc_successrate.py &
+PID3=$!
+sleep 0.5
+echo '' 
+
+python calc_successrate_com.py &
+PID4=$!
+sleep 0.5
+echo '' 
+
+python calc_probability.py &
+PID5=$!
+sleep 0.5
+echo '' 
+
+# Wait for all background processes to complete
+wait $PID1 $PID2 $PID3 $PID4 $PID5
+
+# Run the final Python script after all others are finished
+echo 'Building lists...'
+python build_lists_allbetas.py
